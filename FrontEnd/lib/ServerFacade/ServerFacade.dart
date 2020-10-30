7c2a6f137cab0f4058aff6f5b2ef0ba7f03b1e8e
Tanner Young <young.tannerl@gmail.com>
2020-10-30 02:06:44 -0600

    Add http requests

--- FrontEnd/lib/ServerFacade/ServerFacade.dart
+++ FrontEnd/lib/ServerFacade/ServerFacade.dart
@@ -21,7 +21,7 @@ class ServerFacade {
 
   Future<UserResult> login(LoginRequest r) async {
     final http.Response response = await http.post(
-      "http://" + serverHost + ":" + serverPort + "/users/login",
+      "http://" + serverHost + ":" + serverPort + "/user/login",
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -32,56 +32,160 @@ class ServerFacade {
 
     if (response.statusCode == 201) {
       //Return a UserResult object
+      return UserResult.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to login');
     }
-    return null;
   }
 
-  Future<UserResult> register(RegisterRequest r) {
-    return null;
+  Future<UserResult> register(RegisterRequest r) async {
+    final http.Response response = await http.post(
+      "http://" + serverHost + ":" + serverPort + "/user/register",
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, RegisterRequest>{
+        'registerRequest': r,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return UserResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to login');
+    }
   }
 
-  Future<UserResult> getUser(String username) {
-    return null;
+  Future<UserResult> getUser(String username) async {
+    final response = await http
+        .get("http://" + serverHost + ":" + "serverPort" + "/user/" + username);
+
+    if (response.statusCode == 200) {
+      return UserResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to load user");
+    }
   }
 
-  Future<PlayerResult> getPlayer(int playerId) {
-    return null;
+  Future<PlayerResult> getPlayer(int playerId) async {
+    final response = await http.get("http://" +
+        serverHost +
+        ":" +
+        "serverPort" +
+        "/player/" +
+        playerId.toString());
+
+    if (response.statusCode == 200) {
+      return PlayerResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to load player");
+    }
   }
 
-  Future<PlayersResult> getPlayers(String userId) {
-    return null;
+  Future<PlayersResult> getPlayers(String userId) async {
+    final response = await http.get(
+        "http://" + serverHost + ":" + "serverPort" + "/players/" + userId);
+
+    if (response.statusCode == 200) {
+      return PlayersResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to load players");
+    }
   }
 
-  Future<PlayerResult> joinGame(JoinGameRequest r) {
-    return null;
+  Future<PlayerResult> joinGame(JoinGameRequest r) async {
+    final http.Response response = await http.post(
+      "http://" + serverHost + ":" + serverPort + "/join_game",
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, JoinGameRequest>{
+        'joinGameRequest': r,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return PlayerResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to join game');
+    }
   }
 
-  Future<PlayerResult> leaveGame(int playerId) {
-    return null;
+  Future<PlayerResult> leaveGame(int playerId) async {
+    final response = await http.get("http://" +
+        serverHost +
+        ":" +
+        "serverPort" +
+        "/leave_game/" +
+        playerId.toString());
+
+    if (response.statusCode == 200) {
+      return PlayerResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to leave game");
+    }
   }
 
-  Future<GameResult> startGame(int gameId) {
-    return null;
-  }
-
-  Future<PlayerResult> killTarget(int playerId) {
-    return null;
-  }
-
-  Future<PlayerResult> createGame(GameRequest r) {
-    return null;
-  }
-
-  Future<GameResult> getGame(int gameId) async {
-    String url = "http://" + serverHost + ":" + serverPort + "/game/" + gameId.toString();
-    final response = await http.get(url);
+  Future<GameResult> startGame(int gameId) async {
+    final response = await http.get("http://" +
+        serverHost +
+        ":" +
+        "serverPort" +
+        "/start_game/" +
+        gameId.toString());
 
     if (response.statusCode == 200) {
       return GameResult.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load game');
+      throw Exception("Failed to start game");
+    }
+  }
+
+  Future<PlayerResult> killTarget(int playerId) async {
+    final response = await http.get("http://" +
+        serverHost +
+        ":" +
+        "serverPort" +
+        "/kill_target/" +
+        playerId.toString());
+
+    if (response.statusCode == 200) {
+      return PlayerResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to kill target. You suck");
+    }
+  }
+
+  Future<PlayerResult> createGame(GameRequest r) async {
+    final http.Response response = await http.post(
+      "http://" + serverHost + ":" + serverPort + "/game",
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, GameRequest>{
+        'gameRequest': r,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return PlayerResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create game');
+    }
+  }
+
+  Future<GameResult> getGame(int gameId) async {
+    final response = await http.get("http://" +
+        serverHost +
+        ":" +
+        "serverPort" +
+        "/game/" +
+        gameId.toString());
+
+    if (response.statusCode == 200) {
+      return GameResult.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to load game");
     }
   }
 }
